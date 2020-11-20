4c3a5ddb29de30e77464784499e767758b8beb5c
GabeHaegele <37354142+GabeHaegele@users.noreply.github.com>
2020-11-20 03:10:56 -0600

    Update killer_whale.h

--- killer_whale.h
+++ killer_whale.h
@@ -14,12 +14,29 @@ class killer_whale
     int m_y;
     int m_kills;
   public:
+    //Desc: killer_whale() is the constructor
+    //Pre: 
+    //Post: 
     killer_whale();
-    void move();
+    //Desc: setXY() sets the xy pos
+    //Pre: (x,y) is a valid coord
+    //Post: 
     void setXY(const int x, const int y);
+    //Desc: getX() gets the x pos
+    //Pre: 
+    //Post: returns the x position
     int getX() const;
+    //Desc: getY() gets the y pos
+    //Pre: 
+    //Post: returns the y position
     int getY() const;
+    //Desc: getKills() gets the amounts of kills the whale has
+    //Pre: 
+    //Post: returns the whale's kills
     int getKills() const;
+    //Desc: mov() moves the object in the sea
+    //Pre: sea has data, all arrays have data and the sizes are the amount of objects in their respective array
+    //Post: 
     void mov(sea &sea, penguin pArr[], const int pSize, fish fArr[], const int fSize, killer_whale wArr[], const int wSize);
 };
 #endif
